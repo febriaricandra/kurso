1d4f653f50936cada4247bc486c01044cde83e0b
febriaricandra <febriaricandraproduction@gmail.com>
2023-01-20 00:23:37 +0700

    fix enroll

--- routes/enroll/enroll.controller.js
+++ routes/enroll/enroll.controller.js
@@ -1,29 +1,43 @@
 const User = require("../../models/User");
 const Course = require("../../models/Course");
+const jwt = require("jsonwebtoken");
 
-const enrollCourse = async(req,res) => {
-    const { courseId } = req.body;
-    const { userId } = req.params;
-    try {
-        const user = await User.findById(userId);
-        const course = await Course.findById(courseId);
-        if(!user || !course) {
-            return res.status(400).json({ message: "User or course not found" });
-        }
-        if(!user.courses) user.courses = [];
-        if(user.courses.includes(courseId)) {
-            return res.status(400).json({ message: "User already enrolled in this course" });
-        }
-        user.courses.push(courseId);
-        await user.save();
-        return res.status(200).json({
-            statusCode: 200,
-            message: "User enrolled in course" 
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Server error" });
+const enrollCourse = async (req, res) => {
+    // Check if the request has an authorization header
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-}
+  
+    // Verify the user's JSON web token
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+  
+    // Get the course ID from the request body
+    const courseId = req.body.courseId;
+  
+    try {
+      // Find the user and course by ID
+      const user = await User.findById(userId);
+      const course = await Course.findById(courseId);
+  
+      if (user && course) {
+        // Check if the user is already enrolled in the course
+        const isEnrolled = user.courses.some(c => c._id.toString() === course._id.toString());
+        if (isEnrolled) {
+          return res.status(400).json({ message: "Course already enrolled" });
+        }
+        // Enroll the user in the course
+        user.courses.push(course);
+        await user.save();
+        res.status(200).json({ message: "Successfully enrolled" });
+      } else {
+        res.status(404).json({ message: "User or course not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
 
 module.exports = { enrollCourse };
